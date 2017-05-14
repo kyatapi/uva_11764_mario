30c6bea35414c06f7c881c69fa4992a468799b84
Mike Su <kyatapi@gmail.com>
2017-05-14 16:16:23 +0800

    Add test for first baby step, evaluate test for a single wall(no jump needed)

--- Mario.cpp
+++ Mario.cpp
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <string>
+#include <utility>
 #include <iostream>
 
 #ifndef ONLINE_JUDGE
 #include <fstream>
+#include <gtest\gtest.h>
 #endif
 
 using namespace std;
 
-int main() {
+pair<int, int> EvaluateJumps(const unsigned int wall_count, const string&  wall_list) {
+	return pair<int, int>(0, 0);
+}
+
+TEST(EvaluateJumpsTest, HandleSingleWall) {
+	ASSERT_EQ((pair<int, int>(0, 0)), EvaluateJumps(1, "1"));
+}
+
+int main(int argc, char **argv) {
 #ifndef ONLINE_JUDGE
 	//Redirect stdin/out
 	ifstream in("D:\\Project\\Mario\\Debug\\in.txt");
@@ -21,6 +31,9 @@ int main() {
 #endif // !ONLINE_JUDGE
 
 #ifndef ONLINE_JUDGE
+	::testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
+
 	cin.rdbuf(cinbuf);
 	cout.rdbuf(coutbuf);
 #endif // !ONLINE_JUDGE
